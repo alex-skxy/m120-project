1ddb70986f545848ed8e3238987e2b3b0742ca44
alex-skxy <alex13skx@gmail.com>
2021-06-12 20:31:10 +0200

    fill the table with loan values

--- src/main/java/alexdaniel/m120project/controller/TableController.java
+++ src/main/java/alexdaniel/m120project/controller/TableController.java
@@ -2,29 +2,36 @@ package alexdaniel.m120project.controller;
 
 import alexdaniel.m120project.model.entity.Loan;
 import alexdaniel.m120project.model.repository.LoanRepository;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.util.Optional;
+
 public class TableController {
     @FXML
     public TableView<Loan> loanTable;
+    @FXML
+    public TableColumn<Loan, String> nameColumn;
+    @FXML
+    public TableColumn<Loan, String> movieTitleColumn;
+    @FXML
+    public TableColumn<Loan, String> expectedReturnDateColumn;
+    @FXML
+    public TableColumn<Loan, String> isLateColumn;
 
-    public TableController() {
-        TableColumn<Loan, String> nameColumn = new TableColumn<>("Name");
+    @FXML
+    public void initialize() {
+        fillTable();
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        //TODO make columns
-        /*TableColumn<Loan, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
-        TableColumn<Loan, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        */
-        /*TableColumn<Loan, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        */
+        movieTitleColumn.setCellValueFactory(cellDataFeature -> new SimpleStringProperty(Optional.of(cellDataFeature.getValue().movie.title).orElse("")));
+        // TODO: return date calculation
+        expectedReturnDateColumn.setCellValueFactory(cellDataFeature -> new SimpleStringProperty(""));
+        // TODO: isLate calculation
+        isLateColumn.setCellValueFactory(cellDataFeature -> new SimpleStringProperty(""));
     }
 
     public void fillTable() {
