a1b7c44da8b50f0179f2bd2b8e7f82057972c3a9
alex-skxy <alex13skx@gmail.com>
2021-06-12 20:30:53 +0200

    add getMemberships method to MembershipRepository

--- src/main/java/alexdaniel/m120project/model/repository/MembershipRepository.java
+++ src/main/java/alexdaniel/m120project/model/repository/MembershipRepository.java
@@ -4,6 +4,7 @@ import alexdaniel.m120project.model.entity.Membership;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MembershipRepository {
     private static List<Membership> memberships;
@@ -15,6 +16,10 @@ public class MembershipRepository {
         memberships.add(new Membership(null, "Bronze", 5));
         memberships.add(new Membership(null, "Default", 0));
         memberships.add(new Membership(null, "Piss", -5));
+    }
+
+    public static Membership getMembership(String title) {
+        return memberships.stream().filter(membership -> Objects.equals(membership.title, title)).findFirst().orElse(memberships.get(3));
     }
 
     private static void createMembership(Membership membership) {
