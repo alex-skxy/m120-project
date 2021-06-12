faccc1ffe5273a132494e0e4066622b4950da7f6
alex-skxy <alex13skx@gmail.com>
2021-06-12 20:30:23 +0200

    add some test data

--- src/main/java/alexdaniel/m120project/Main.java
+++ src/main/java/alexdaniel/m120project/Main.java
@@ -1,12 +1,15 @@
 package alexdaniel.m120project;
 
+import alexdaniel.m120project.model.entity.Loan;
+import alexdaniel.m120project.model.entity.Movie;
+import alexdaniel.m120project.model.repository.LoanRepository;
+import alexdaniel.m120project.model.repository.MembershipRepository;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -15,6 +18,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        // TODO: remove test data after testing
+        LoanRepository.createMovie(new Loan(null, "alex", new Date(),
+                new Movie(null, "A wild ride to down to hell on a stripper pole while being sexy and gay, fuck yeah, this is lil nas x"),
+                MembershipRepository.getMembership("Piss"), false));
+        LoanRepository.createMovie(new Loan(null, "daniel", new Date(),
+                new Movie(null, "Kumo desu ga nani ka: into the spiderverse"),
+                MembershipRepository.getMembership("Silver"), false));
+
         Stage root = FXMLLoader.load(getClass().getResource("view/tabledisplay.fxml"));
 
         stage = root;
