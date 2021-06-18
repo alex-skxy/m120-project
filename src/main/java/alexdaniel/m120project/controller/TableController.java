7dd3551686bfff6dc548ce1203862d8ae01ff7da
alex-skxy <alex13skx@gmail.com>
2021-06-18 08:45:16 +0200

    update test data to include overdue date, show return date and is late field

--- src/main/java/alexdaniel/m120project/controller/TableController.java
+++ src/main/java/alexdaniel/m120project/controller/TableController.java
@@ -13,6 +13,9 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
 
 public class TableController {
@@ -31,11 +34,41 @@ public class TableController {
     public void initialize() {
         fillTable();
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        movieTitleColumn.setCellValueFactory(cellDataFeature -> new SimpleStringProperty(Optional.of(cellDataFeature.getValue().movie.title).orElse("")));
+        movieTitleColumn.setCellValueFactory(
+                cellDataFeature -> new SimpleStringProperty(Optional.of(cellDataFeature.getValue().movie.title).orElse(""))
+        );
+
+        var dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+
         // TODO: return date calculation
-        expectedReturnDateColumn.setCellValueFactory(cellDataFeature -> new SimpleStringProperty(""));
+        expectedReturnDateColumn.setCellValueFactory(
+                cellDataFeature -> new SimpleStringProperty(
+                        dateFormatter.format(calculateReturnDate(
+                                cellDataFeature.getValue().date,
+                                cellDataFeature.getValue().membership.extraDays + 30).getTime()
+                        )
+                ));
         // TODO: isLate calculation
-        isLateColumn.setCellValueFactory(cellDataFeature -> new SimpleStringProperty(""));
+        isLateColumn.setCellValueFactory(
+                cellDataFeature -> new SimpleStringProperty(isLate(calculateReturnDate(
+                        cellDataFeature.getValue().date,
+                        cellDataFeature.getValue().membership.extraDays + 30)) ? "late" : "not late")
+        );
+    }
+
+    private Boolean isLate(Calendar returnDate) {
+        var today = Calendar.getInstance();
+        today.setTime(new Date());
+
+        return returnDate.compareTo(today) < 0;
+    }
+
+    private Calendar calculateReturnDate(Date loanDate, int extraDays) {
+        var calendar = Calendar.getInstance();
+        calendar.setTime(loanDate);
+        calendar.add(Calendar.DAY_OF_MONTH, extraDays);
+
+        return calendar;
     }
 
     public void fillTable() {
